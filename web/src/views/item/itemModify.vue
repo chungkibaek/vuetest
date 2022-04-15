dacaac839d9bbe36e5c9b250657bffa29923e221
chungkibaek <chungki.baek81@gmial.com>
2022-04-15 15:19:18 +0900

    g

---
+++ web/src/views/item/itemModify.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{sampleData}}
+  </div>
+</template>
+<script>
+export  default{
+  name :'',
+  components:{},
+  data(){
+    return {
+      sampleData : 'ITMEModify'
+    };
+  },
+  watch : {},
+  computed : {},
+  created(){},
+  mounted(){},
+  unmounted(){},
+  methods:{}
+}
+</script>
